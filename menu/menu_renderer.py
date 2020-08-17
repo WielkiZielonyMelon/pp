703ba3da9b6ca2d427a24f751db34861139cad3f
wielkizielonymelon <wielkizielonymelon@gmail.com>
2020-08-17 16:05:55 +0200

    Changed font size

--- menu/menu_renderer.py
+++ menu/menu_renderer.py
@@ -1,6 +1,6 @@
 import pygame
 
-default_font_size = 20
+default_font_size = 48
 menu_font = pygame.font.SysFont(pygame.font.get_default_font(), default_font_size)
 active_menu_selection_color = (50, 255, 50)
 inactive_menu_selection_color = (50, 50, 50)
@@ -14,8 +14,10 @@ class MenuRenderer:
 
     def render_menu_item(self, menu_item, index, active):
         y_offset = (index + 1) * self.menu_spacing
+        # Erase previous text
         self.screen.fill(pygame.Color("black"), (0, y_offset, self.screen.get_width(),
                                                  default_font_size))
+
         surface = menu_font.render(menu_item.name, True,
                                    active_menu_selection_color if active else inactive_menu_selection_color)
 
